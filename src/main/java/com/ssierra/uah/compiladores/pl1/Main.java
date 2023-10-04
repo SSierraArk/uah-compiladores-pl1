de1d689ca124da4cdd27bb5e1ab1760a4c473c93
Sergio <sergio.sierra@edu.uah.es>
2023-10-04 22:47:36 +0200

    Added NetBeans Project

---
+++ src/main/java/com/ssierra/uah/compiladores/pl1/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ssierra.uah.compiladores.pl1;
+
+/**
+ *
+ * @author ssierra
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
